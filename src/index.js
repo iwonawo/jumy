e3dcfa1afc6599ccc3f0382cc8a9d7de7ed0a923
iwonawo <wozniakowskaiwona@gmail.com>
2021-12-04 14:48:27 +0400

    add fonts and images

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+//styles
+import './index.scss'
+
+//fonts
+import './fonts/Gilroy/Gilroy-Medium.ttf'
+import './fonts/Gilroy/Gilroy-SemiBold.ttf'
+
+//components
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
